559a7863dd253448d5b349110681a7139d2df366
syz <eagle-china@hotmail.com>
2018-10-17 11:07:22 +0800

    change test case

--- example/test.dart
+++ example/test.dart
@@ -9,10 +9,12 @@ Future<Null> main() async {
     var httpClient = new Client();
     Web3Client client = new Web3Client(_URL, httpClient);
     client.printErrors = true;
-    var txs = await client.getTransactionsByBlockNumber(380, 0, 10);
+    var txs = await client.getTransactionsByBlockNumber(428, 0, 10);
     // var txs = await client.getTransactions(EthereumAddress("0x220429a6c8bdf58b9c65b4fd5b2b245d79f112de"), TransactionType.income, 0, 10);
     print(txs.total);
     for (var i = 0; i < txs.datas.length; i++) {
-      print(txs.datas[i].toJson());
+      print(txs.datas[i].gasUsed.getValueInUnit(EtherUnit.ether).toString());
+      print(txs.datas[i].amount.getValueInUnit(EtherUnit.ether).toString());
+      print(txs.datas[i].gasPrice.getValueInUnit(EtherUnit.wei).toString());
     }
 }
